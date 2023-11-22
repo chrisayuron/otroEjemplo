30d5159dcddec4402b6acceb16c2d0d289c706dc
Pepito Perez <Pepito@nose.com>
2023-11-22 10:29:50 -0500

    codigo de suma de 2 valores

--- python.py
+++ python.py
@@ -1,1 +1,8 @@
 #Hola, este archivo va a iniciar su seguimiento
+try: 
+    valA=int(input("Digita un número: "))
+    valB=int(input("Digita otro número: "))
+    suma=valA+valB
+    print(suma)
+except:
+    print("Dato mal ingresado")
